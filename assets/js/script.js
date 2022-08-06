e85e3c8f9ba341781baaf8af4c527b3b690670c9
aulianurfajriyah <aulia.nur.fajriyah@mail.ugm.ac.id>
2022-08-06 14:23:48 +0700

    landing page

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("asl_translator").addEventListener("click", aslTranslator);
+      function aslTranslator() {
+        window.location.href="asl__translator.html";
+      }
+
+document.getElementById("asl_recognition").addEventListener("click", aslRecognition);
+      function aslRecognition() {
+        window.location.href="asl__recognition.html";
+      }

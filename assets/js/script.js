fbb627e0ad12a0a6caf80e9e9f81c1ad0ad438dd
aulianurfajriyah <aulia.nur.fajriyah@mail.ugm.ac.id>
2022-08-06 15:17:00 +0700

    navbar added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-document.getElementById("asl_translator").addEventListener("click", aslTranslator);
+document.getElementById("asl__translator").addEventListener("click", aslTranslator);
       function aslTranslator() {
         window.location.href="asl__translator.html";
       }
 
-document.getElementById("asl_recognition").addEventListener("click", aslRecognition);
+document.getElementById("asl__recognition").addEventListener("click", aslRecognition);
       function aslRecognition() {
         window.location.href="asl__recognition.html";
       }

fe0e19ad4a3bf42b8893b189c287a6b8d1a3b0fd
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-08-10 23:44:48 +0700

    style: prettier

--- assets/js/speechToText.js
+++ assets/js/speechToText.js
@@ -1,33 +1,33 @@
 let micOn = false;
 
-if (!('webkitSpeechRecognition' in window)) {
+if (!("webkitSpeechRecognition" in window)) {
   alert(`Speech to text doesn't work on this browser`);
 } else {
   // Initialize webkitSpeechRecognition
   let speechRecognition = new webkitSpeechRecognition();
 
   // String for the Final Transcript
-  let final_transcript = '';
+  let final_transcript = "";
 
   // Set the properties for the Speech Recognition object
   speechRecognition.continuous = true;
   speechRecognition.interimResults = true;
   // speechRecognition.lang = document.querySelector('#select_dialect').value;
-  speechRecognition.lang = 'en-US';
+  speechRecognition.lang = "en-US";
 
-  const langIdButton = document.getElementById('lang-id');
-  const langEnButton = document.getElementById('lang-en');
+  const langIdButton = document.getElementById("lang-id");
+  const langEnButton = document.getElementById("lang-en");
 
-  langIdButton.addEventListener('click', () => {
-    speechRecognition.lang = 'id-ID';
-    langIdButton.classList.add('lang-item-active');
-    langEnButton.classList.remove('lang-item-active');
+  langIdButton.addEventListener("click", () => {
+    speechRecognition.lang = "id-ID";
+    langIdButton.classList.add("lang-item-active");
+    langEnButton.classList.remove("lang-item-active");
   });
 
-  langEnButton.addEventListener('click', () => {
-    speechRecognition.lang = 'en-US';
-    langEnButton.classList.add('lang-item-active');
-    langIdButton.classList.remove('lang-item-active');
+  langEnButton.addEventListener("click", () => {
+    speechRecognition.lang = "en-US";
+    langEnButton.classList.add("lang-item-active");
+    langIdButton.classList.remove("lang-item-active");
   });
 
   // // Callback Function for the onStart Event
@@ -46,7 +46,7 @@ if (!('webkitSpeechRecognition' in window)) {
 
   speechRecognition.onresult = (event) => {
     // Create the interim transcript string locally because we don't want it to persist like final transcript
-    let interim_transcript = '';
+    let interim_transcript = "";
 
     // Loop through the results from the speech recognition object.
     for (let i = event.resultIndex; i < event.results.length; ++i) {
@@ -59,7 +59,7 @@ if (!('webkitSpeechRecognition' in window)) {
     }
 
     // Set the Final transcript and Interim transcript.
-    document.querySelector('#inputText').value = final_transcript;
+    document.querySelector("#inputText").value = final_transcript;
     console.log(final_transcript);
     // document.querySelector('#interim').innerHTML = interim_transcript;
   };
@@ -75,14 +75,14 @@ if (!('webkitSpeechRecognition' in window)) {
   //   speechRecognition.stop();
   // };
 
-  const micButton = document.querySelector('#start');
+  const micButton = document.querySelector("#start");
 
-  micButton.addEventListener('click', () => {
+  micButton.addEventListener("click", () => {
     micOn = !micOn;
-    micButton.classList.toggle('mic-on');
+    micButton.classList.toggle("mic-on");
 
     if (micOn) {
-      final_transcript = '';
+      final_transcript = "";
       speechRecognition.start();
     } else {
       speechRecognition.stop();
